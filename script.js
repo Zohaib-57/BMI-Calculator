056a14432eb483f8dcb52b5bd02d8b5cae500ce6
Your Name <zohaibabbas8557@gmail.com>
2024-10-11 16:46:36 +0500

    checking the valid input values

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function calculateBMI(){
+    // Selecting the input values...
     let name = document.getElementById("name").value;
     let age = document.getElementById("age").value;
      let height = document.getElementById("height").value/100;
      let weight = document.getElementById("weight").value;
      
+    //  checking the valid input
+    if (!name || !age || !height || !weight) {
+        alert('Please fill in all the fields.');
+        return;
+    }
 }
