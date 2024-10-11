637d5b643f9ca970b9c81bd6dd2b72e46002f948
Your Name <zohaibabbas8557@gmail.com>
2024-10-11 09:04:30 +0500

    added the script and selected the input

---
+++ script.js
@@ -0,0 +1,7 @@
+function calculateBMI(){
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+     let height = document.getElementById("height").value/100;
+     let weight = document.getElementById("weight").value;
+     
+}
